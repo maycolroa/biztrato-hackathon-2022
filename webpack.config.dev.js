97ae314d28636d419e8141e08163a1e3174aaf0d
Carlos Revetti <cdrn_19@hotmail.com>
2022-11-16 20:25:13 -0400

    :heavy_plus_sign: added webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,76 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+        "@api": path.resolve(__dirname, "src/services/api"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@containers": path.resolve(__dirname, "src/containers"),
+        "@context": path.resolve(__dirname, "src/context"),
+        "@hooks": path.resolve(__dirname, "src/hooks"),
+        "@pages": path.resolve(__dirname, "src/pages"),
+        "@images": path.resolve(__dirname, "src/assets/images"),
+        "@icons": path.resolve(__dirname, "src/assets/icons"),
+        "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg)$/,
+        type: "asset",
+      },
+    ],
+  },
+  devtool: false,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new Dotenv(),
+    new webpack.SourceMapDevToolPlugin({}),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+  mode: "production",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@api": path.resolve(__dirname, "src/services/api"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader","css-loader"]
+      },
+      {
+        test: /\.(png|svg)$/,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        API: JSON.stringify(process.env.API),
+        VERSION: JSON.stringify(process.env.VERSION),
+      },
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        terserOptions: { sourceMap: true },
+      }),
+    ],
+  },
+};
